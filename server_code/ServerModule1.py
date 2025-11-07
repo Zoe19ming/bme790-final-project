45ecd7247e0a4eaba973925d8ce21e4220cfbe9a
sx107@duke.edu <sx107@duke.edu>
2025-11-07 04:29:32 +0000

    Edited ServerModule1, Form2 and custom package dependencies

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -7,8 +7,9 @@ from io import BytesIO
 def ping(): return "pong"
   
 def process_uploaded_csv(file):
+  
   # Read uploaded CSV into pandas DataFrame
-  Raw_data = pd.read_csv(BytesIO(file.get_bytes()))
+  Raw_data = pd.read_csv(file,encoding = "ISO-8859-1")
   #missing_counts = Raw_data.isnull().sum()
   Cleaned_raw = Raw_data.dropna(axis = 1, thresh = int(0.8*len(Raw_data)))
   cols_to_drop = ['cancSpec FAMILY HISTORY',"Hdspecific FAMILY HISTORY","HTNspecific FAMILY HISTORY",
@@ -17,5 +18,5 @@ def process_uploaded_csv(file):
   Cleaned_raw = Cleaned_raw.drop(columns = cols_to_drop)  
 
   # Convert to list of dicts for display in DataGrid
-  df_dict = Raw_data.to_dict(orient='records')
+  df_dict = Cleaned_raw.to_dict(orient='records')
   return df_dict
